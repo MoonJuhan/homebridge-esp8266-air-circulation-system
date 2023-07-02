8542b05b53c18bfb3e8340f7a7e88e10ea8cf789
MoonJuhan <moonjuhan@gimmingyeong-ui-MacBookAir.local>
2023-07-02 21:20:55 +0900

    feat: Add relay module files

---
+++ arduino-sketch/esp8266-relay-module-web-server.ino
@@ -0,0 +1,83 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <ArduinoJson.h>
+
+const char *ssid = "YourNetworkSSID";
+const char *password = "YourNetworkPassword";
+const int relayPin = "YourPinNumber";
+
+ESP8266WebServer server(80);
+
+void handleRoot()
+{
+    server.send(200, "text/plain", "Hello from ESP8266!");
+}
+
+void handleGet()
+{
+    server.send(200, "text/plain", "Online");
+}
+
+void setRelayModule(String relayOn)
+{
+    pinMode(relayPin, OUTPUT);
+
+    if (relayOn == "true")
+    {
+        digitalWrite(relayPin, LOW);
+    }
+    else
+    {
+        digitalWrite(relayPin, HIGH);
+    }
+}
+
+void handlePut()
+{
+    String jsonString = server.arg("plain");
+
+    DynamicJsonDocument jsonDocument(1024);
+    DeserializationError error = deserializeJson(jsonDocument, jsonString);
+
+    if (error == DeserializationError::Ok)
+    {
+        String relayOn = jsonDocument["relayOn"].as<String>();
+        setRelayModule(relayOn);
+
+        server.send(200, "text/plain", "Success");
+    }
+    else
+    {
+        server.send(400, "text/plain", "Failed to parse JSON data");
+    }
+}
+
+void setup()
+{
+    Serial.begin(115200);
+
+    pinMode(relayPin, OUTPUT);
+    digitalWrite(relayPin, HIGH);
+
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(1000);
+        Serial.print(".");
+    }
+
+    Serial.println("Wi-Fi connected");
+    Serial.println("IP address: " + WiFi.localIP().toString());
+
+    server.on("/", handleRoot);
+    server.on("/relay", HTTP_GET, handleGet);
+    server.on("/relay", HTTP_PUT, handlePut);
+
+    server.begin();
+    Serial.println("HTTP server started");
+}
+
+void loop()
+{
+    server.handleClient();
+}
---
+++ src/index.js
@@ -0,0 +1,86 @@
+'use strict'
+
+const axios = require('axios')
+const dayjs = require('dayjs')
+
+const setup = (homebridge) => {
+  homebridge.registerAccessory('homebridge-esp8266-air-circuration-system', 'ESP8266RelayModule', ESP8266RelayModule)
+}
+
+class ESP8266RelayModule {
+  constructor(log, config, api) {
+    log('ESP8266RelayModule start.')
+
+    const { Service, Characteristic } = api.hap
+
+    const { name, ip, cacheTime } = config
+    this.log = log
+    this.ip = ip
+    this.cacheTime = cacheTime || 20
+
+    log(`Name : ${name}, IP : ${ip}`)
+
+    this.informationService = new Service.AccessoryInformation()
+      .setCharacteristic(Characteristic.Manufacturer, 'Custom Manufacturer')
+      .setCharacteristic(Characteristic.Model, 'Custom Model')
+
+    this.switchService = new Service.Switch(name)
+    this.switchService
+      .getCharacteristic(Characteristic.On)
+      .onGet(this.getOnHandler.bind(this))
+      .onSet(this.setOnHandler.bind(this))
+
+    this.getRelayStatusTime = null
+    this.relayOn = false
+
+    this.setOnHandler(false)
+  }
+
+  getServices() {
+    return [this.informationService, this.switchService]
+  }
+
+  async getOnHandler() {
+    this.log.info('Get relay module status.')
+
+    if (dayjs().diff(this.getRelayStatusTime, 'second', true) < cacheTime) {
+      return this.relayOn
+    }
+
+    try {
+      this.log.info('Call get relay status api.')
+
+      this.getRelayStatusTime = new Date()
+      await axios({
+        url: '/relay',
+        baseURL: `http://${this.ip}`,
+        method: 'get',
+      })
+    } catch (error) {
+      this.log.error('Axios Error')
+      this.relayOn = false
+    } finally {
+      return this.relayOn
+    }
+  }
+
+  async setOnHandler(value) {
+    this.log.info('Set relay module:', value)
+
+    try {
+      await axios({
+        url: '/relay',
+        baseURL: `http://${this.ip}`,
+        method: 'put',
+        data: { relayOn: value },
+      })
+
+      this.relayOn = value
+      this.log.info('Set relay module success.')
+    } catch (error) {
+      this.log.error('Set relay module failed.')
+    }
+  }
+}
+
+module.exports = setup
